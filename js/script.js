bbe8009f890c142975f7622961208f9be6c096b0
antistian <78101982+antistian@users.noreply.github.com>
2021-08-29 21:59:36 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Question 1
+
+const getRemainder = (a, b) => a % b;
+
+//Question 2
+
+console.log("I gave up, didn't understand why nothing showed up from the API, deleted all the code and I'm getting a panic attack and concidering throwing my Macbook out of the window.")
